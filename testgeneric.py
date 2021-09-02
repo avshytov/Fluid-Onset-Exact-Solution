f185d3cd81d831059bbdc428292b4d7ebd02a032
avshytov <avshytov@gmail.com>
2021-09-02 22:24:15 +0100

    clearer printout

--- testgeneric.py
+++ testgeneric.py
@@ -33,16 +33,16 @@ def test(flow, label):
                 "up: rho+", label)
     do_compare (x_arc, gen_flow.Omega_plus_up(), flow.Omega_plus_up(),
                 "up: Omega+", label)
-    print ("fluxes:", flow.wall_flux(), gen_flow.wall_flux())
+    print ("fluxes: orig = ", flow.wall_flux(), "gen = ", gen_flow.wall_flux())
     y = np.linspace(-1, 20, 2101)
     do_compare(y, gen_flow.rho_y(y), flow.rho_y(y), "rho(y)", label)
     do_compare(y, gen_flow.jx_y(y), flow.jx_y(y), "j_x(y)", label)
     do_compare(y, gen_flow.jy_y(y), flow.jy_y(y), "j_y(y)", label)
 
 k = -0.5
-h = 0.8
+h = 0.05
 gamma  = 1.0
-gamma1 = 0.5
+gamma1 = 1.0
 
 K = WHKernels(gamma, gamma1)
 path_up, K_up, path_dn, K_dn = make_paths_and_kernels(K, k)
@@ -62,13 +62,13 @@ log_Ko_star = cauchy_integral_array(path_up, np.log(K.omega(k, path_dn.points())
 #print ("dn: log Ko_star = ", log_Ko_star, np.log(K_up.Komega_star))
 
 custom_flows = {
+    "stokes-x" : Stokeslet(1, 0, h, k, K_up, path_up, K_dn, path_dn),
+    "stokes-y" : Stokeslet(0, 1, h, k, K_up, path_up, K_dn, path_dn),
     "edge-src-sym" : EdgeInjectedFlow_sym(k, K_up, path_up, K_dn, path_dn),
     "diffuse-sym"  : DiffuseFlow_sym(k, K_up, path_up, K_dn, path_dn),
     "diffuse"  : DiffuseFlow(k, K_up, path_up, K_dn, path_dn),
     "edge-src" : EdgeInjectedFlow(k, K_up, path_up, K_dn, path_dn),
     "bulk-src" : InjectedFlow(h, k, K_up, path_up, K_dn, path_dn),
-    "stokes-x" : Stokeslet(1, 0, h, k, K_up, path_up, K_dn, path_dn),
-    "stokes-y" : Stokeslet(0, 1, h, k, K_up, path_up, K_dn, path_dn),
 }
 
 for label, flow in custom_flows.items():
